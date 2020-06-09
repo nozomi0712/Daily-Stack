3474cb6fb52e622944beaf461599a12c29aff727
nozomi0712 <s.mo.19930712@gmail.com>
2020-06-09 18:25:02 +0900

    migrate

--- db/migrate/20200430135354_create_posts.rb
+++ db/migrate/20200430135354_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.string :title,   null: false
-      t.text :text,      null: false
+      t.text :textn,      null: false
       t.text :image
       t.integer :grade,   null: false
       t.string :subject, null: false
--- db/migrate/20200609090311_rename_column.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumn < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :posts, :text, :textn
-  end
-end
