7bd13b66727af104dc1971468a28bc91fd6b16c0
nozomi0712 <s.mo.19930712@gmail.com>
2020-06-12 21:33:40 +0900

    setting

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -5,9 +5,11 @@ class ImagesUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [350, 350]
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
-
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -36,9 +38,9 @@ class ImagesUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,15 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  elsif Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    :aws_access_key_id     => ENV["AWS_ACCESS_KEY_ID"],
+    :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1'
   }
 
