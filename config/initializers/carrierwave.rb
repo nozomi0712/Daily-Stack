8df4feb6d4328f25312b227204683349ec7cdd57
nozomi0712 <s.mo.19930712@gmail.com>
2020-06-12 21:41:07 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,11 +9,12 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: 'ap-northeast-1'
-  }
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'ap-northeast-1'
+    }
+  end
 
   config.fog_directory  = 'daily-stack'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/daily-stack'
