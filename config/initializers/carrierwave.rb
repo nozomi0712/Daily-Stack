8770c320c777f21afd2d6c0a2f5fb8671801af67
nozomi0712 <s.mo.19930712@gmail.com>
2020-06-13 17:38:46 +0900

    o

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,13 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  elsif Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'  #=> 追加
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region: 'ap-northeast-1'
     }
   end
